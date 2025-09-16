c65591e3967d1349c63f2b42376e675e5601e41d
DaoChaShao <27882272@qq.com>
2025-09-16 18:51:02 +0800

    feat: create a file for practising how to use the variables

---
+++ variables.m
@@ -0,0 +1,13 @@
+% MATLAB Ver. : 25.1.0.2973910 (R2025a) Update 1
+% Style Guide : MATLAB Style Guidelines 1.6
+% Encoding    : ***** UTF-8 *****
+% File        : variables.m
+% Author      : daochashao
+% Date        : 2025-09-16 18:03:49
+% Code Ver.   : 0.1.0
+% Desc        : 
+
+a = 10;
+b = 10;
+resultSum = a + b;
+disp(resultSum)
