45871f25c76d1f4efe846444b240e8b6afad8d6d
DaoChaShao <27882272@qq.com>
2025-09-16 23:33:30 +0800

    feat: update the functions in the Python file

--- utils.py
+++ utils.py
@@ -1,2 +1,9 @@
+from random import choice
+
 def say_hi(name: str):
     return f"Hello {name}, This is from Python!"
+
+def colour_getter() -> str:
+    colours: list[str] = ["red", "blue"]
+    return choice(colours)
+
