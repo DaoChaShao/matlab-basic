a38dae52bc8fb331c777b7a3c45541aac4d89393
DaoChaShao <27882272@qq.com>
2025-09-17 10:56:23 +0800

    feat: create a new function of subtraction

---
+++ +helper/subtraction.m
@@ -0,0 +1,12 @@
+% MATLAB Ver. : 25.1.0.2973910 (R2025a) Update 1
+% Style Guide : MATLAB Style Guidelines 1.6
+% Encoding    : ***** UTF-8 *****
+% File        : subtraction.m
+% Author      : daochashao
+% Date        : 2025-09-17 10:30:31
+% Code Ver.   : 0.1.0
+% Desc        : 
+
+function [result] = subtraction(x, y)
+    result = x - y;
+end
