ce4767768615982c59bb8b8e5cbd611cca572258
DaoChaShao <27882272@qq.com>
2025-09-17 10:54:19 +0800

    feat: create a new function of addition

---
+++ +helper/addition.m
@@ -0,0 +1,12 @@
+% MATLAB Ver. : 25.1.0.2973910 (R2025a) Update 1
+% Style Guide : MATLAB Style Guidelines 1.6
+% Encoding    : ***** UTF-8 *****
+% File        : add.m
+% Author      : daochashao
+% Date        : 2025-09-17 09:55:07
+% Code Ver.   : 0.1.0
+% Desc        : 
+
+function [result] = addition(x, y)
+    result = x + y;
+end
