a36a96e97d0622a4c87621d5fd2da5fedbf674fc
DaoChaShao <27882272@qq.com>
2025-09-17 10:55:08 +0800

    feat: create a new function of division

---
+++ +helper/division.m
@@ -0,0 +1,12 @@
+% MATLAB Ver. : 25.1.0.2973910 (R2025a) Update 1
+% Style Guide : MATLAB Style Guidelines 1.6
+% Encoding    : ***** UTF-8 *****
+% File        : division.m
+% Author      : daochashao
+% Date        : 2025-09-17 10:31:23
+% Code Ver.   : 0.1.0
+% Desc        : 
+
+function [result] = division(x, y)
+    result = x / y;
+end
